12cce699b02bf8db0ad50d5ee88cf7f096c44d19
manishahirrao <manishkishor2003@gmail.com>
2025-05-12 20:27:27 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
+  server: {
+    port: 3000,
+    strictPort: true
+  }
 })
